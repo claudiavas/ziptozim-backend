357a297e22bcbb676154e2c44f73a071da564ace
Claudia Vasquez <claudia.vasquez.as@gmail.com>
2024-05-23 21:36:57 +0200

    refactor: Improve file upload and conversion process in convertController.js

--- index.test.js
+++ index.test.js
@@ -1,21 +1,30 @@
+const express = require('express');
 const request = require('supertest');
 const app = require('./index'); // Importa tu app de Express
 
-describe('Test /upload endpoint', () => {
-    it('should upload and unzip the file', async () => {
-        const res = await request(app)
-            .post('/upload')
-            .attach('inputFile', 'path/to/your/test.zip'); // Reemplaza 'path/to/your/test.zip' con la ruta a tu archivo de prueba
+// Verifica si la aplicación ya está escuchando en un puerto
+if (!app.listening) {
+    app.listen(0); // 0 significa que el sistema operativo asignará un puerto aleatorio disponible
+}
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty('tempDir');
-        expect(res.body.message).toEqual('File uploaded and unzipped.');
+const agent = request.agent(app); // Pasa la aplicación a Supertest
+
+describe('Test /upload endpoint', () => {
+    it('should upload and unzip the file', () => { // Elimina 'async'
+        return agent // Devuelve la promesa
+            .post('/upload')
+            .attach('inputFile', 'path/to/your/test.zip') // Reemplaza 'path/to/your/test.zip' con la ruta a tu archivo de prueba
+            .then(res => { // Maneja la respuesta en un bloque 'then'
+                expect(res.statusCode).toEqual(200);
+                expect(res.body).toHaveProperty('tempDir');
+                expect(res.body.message).toEqual('File uploaded and unzipped.');
+            });
     });
 });
 
 describe('Test /convert endpoint', () => {
-    it('should start the zip file conversion', async () => {
-        const res = await request(app)
+    it('should start the zip file conversion', () => { // Elimina 'async'
+        return agent // Devuelve la promesa
             .post('/convert')
             .send({
                 tempDir: 'path/to/your/tempDir', // Reemplaza 'path/to/your/tempDir' con la ruta a tu directorio temporal
@@ -27,9 +36,10 @@ describe('Test /convert endpoint', () => {
                 description: ' ',
                 creator: 'Wikipedia',
                 publisher: 'Me'
+            })
+            .then(res => { // Maneja la respuesta en un bloque 'then'
+                expect(res.statusCode).toEqual(200);
+                expect(res.text).toEqual('Zip file conversion started.');
             });
-
-        expect(res.statusCode).toEqual(200);
-        expect(res.text).toEqual('Zip file conversion started.');
     });
 });
