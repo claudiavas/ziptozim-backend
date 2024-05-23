4952bf7786bf3ab61c4fa6508835c2de35f9763d
Claudia Vasquez <claudia.vasquez.as@gmail.com>
2024-05-23 19:41:53 +0200

    chore: Update zimwriterfsPath to use local zimwriterfs executable

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const zimWriters = require('./zimWriters');
 
 const app = express();
 const port = 3019;
-zimWriters()
+
+//Parameter for testing
+const sourceDirectory = './ourwebsite'; // El directorio con los archivos que quieres incluir en el archivo ZIM
+const outputFile = './OurWebsite.zim'; // La ruta donde quieres guardar el archivo ZIM
+const zimwriterfsPath = './zimwriterfs'; // La ruta al ejecutable zimwriterfs
+const welcomePage = 'index.html'; // La página de bienvenida
+const favicon = 'favicon.png'; // La ilustración
+const language = 'eng'; // El código de idioma
+const title = 'Our Website'; // El título del ZIM
+const description = ' '; // La descripción del ZIM
+const creator = 'Wikipedia'; // El creador del ZIM
+const publisher = 'Me'; // El editor del ZIM
+
+createZimFile(sourceDirectory, outputFile, welcomePage, favicon, language, title, description, creator, publisher);
+
 app.use(express.json());
 
 app.listen(port, () => {
--- zimWriters.js
+++ zimWriters.js
@@ -1,22 +1,8 @@
 const { exec } = require('child_process');
 
-const sourceDirectory = './ourwebsite'; // El directorio con los archivos que quieres incluir en el archivo ZIM
-const outputFile = './OurWebsite.zim'; // La ruta donde quieres guardar el archivo ZIM
-const zimwriterfsPath = './zimwriterfs'; // La ruta al ejecutable zimwriterfs
-
-const welcomePage = 'index.html'; // La página de bienvenida
-const favicon = 'favicon.png'; // La ilustración
-const language = 'eng'; // El código de idioma
-const title = 'Our Website'; // El título del ZIM
-const description = ' '; // La descripción del ZIM
-const creator = 'Wikipedia'; // El creador del ZIM
-const publisher = 'Me'; // El editor del ZIM
-
-const command = `${zimwriterfsPath} --welcome=${welcomePage} --favicon=${favicon} --language=${language} --title="${title}" --description="${description}" --creator=${creator} --publisher=${publisher} ${sourceDirectory} ${outputFile}`;
-
 function createZimFile(sourceDirectory, outputFile, welcomePage, favicon, language, title, description, creator, publisher) {
     const command = `${zimwriterfsPath} --welcome=${welcomePage} --favicon=${favicon} --language=${language} --title="${title}" --description="${description}" --creator=${creator} --publisher=${publisher} ${sourceDirectory} ${outputFile}`;
-
+//a
     exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error al crear el archivo ZIM: ${error}`);
